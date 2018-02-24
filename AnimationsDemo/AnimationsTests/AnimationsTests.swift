d1ca00ea2fe2d06ccb270d846b5b49d6e62caa95
Miroslav Sliacky <micromanc3r@gmail.com>
2018-02-24 22:35:24 +0100

    Add basic tests
    Add tests to verify basic functionality. Since animations are
    asynchronous we have to use appropriate method, in this case
    we use expectations.

--- AnimationsDemo/AnimationsTests/AnimationsTests.swift
+++ AnimationsDemo/AnimationsTests/AnimationsTests.swift
@@ -21,10 +21,64 @@ class AnimationsTests: XCTestCase {
     
     func testGracefulHide() {
         let button = UIButton()
+        let expectation = self.expectation(description: "hide")
+        
         XCTAssertTrue(button.alpha == 1.0)
+        
         button.hideGracefully(completion: { _ in
-            XCTAssertTrue(button.alpha == 0.0)
+            expectation.fulfill()
         })
+        
+        waitForExpectations(timeout: 1, handler: nil)
+        XCTAssertTrue(button.alpha == 0.0)
     }
     
+    func testGracefulShow() {
+        let button = UIButton()
+        let expectation = self.expectation(description: "show")
+        
+        button.alpha = 0.0
+        button.showGracefully(completion: { _ in
+            expectation.fulfill()
+        })
+        
+        waitForExpectations(timeout: 1, handler: nil)
+        XCTAssertTrue(button.alpha == 1.0)
+    }
+    
+    func testGracefulAlpha() {
+        let button = UIButton()
+        let expectation = self.expectation(description: "alpha")
+        
+        XCTAssertTrue(button.alpha == 1.0)
+        
+        button.setAlphaGracefully(alpha: 0.5,
+                                  duration: 0.5,
+                                  delay: 0,
+                                  options: [.curveEaseIn]) { _ in
+                                    expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1, handler: nil)
+        XCTAssertTrue(button.alpha == 0.5)
+    }
+    
+    func testGracefulSetImage() {
+        let firstImageName = "widow01"
+        let firstImage = UIImage.init(named: firstImageName)
+        let secondImageName = "widow02"
+        let secondImage = UIImage.init(named: secondImageName)
+        
+        let imageView = UIImageView.init(image: firstImage)
+        
+        let expectation = self.expectation(description: "image")
+        XCTAssertEqual(imageView.image, firstImage)
+        
+        imageView.setImageGracefully(image: secondImage!) { _ in
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 0.5, handler: nil)
+        XCTAssertEqual(imageView.image, secondImage)
+    }
 }
